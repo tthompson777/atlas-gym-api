d4b32dee348687468cdcc93b8ece5951ef64463b
Tiago Thompson <rigothompsontiago@gmail.com>
2026-02-06 05:35:43 -0300

    removendo firebase.ts

--- src/firebase.ts
+++
@@ -1,8 +0,0 @@
-import admin from 'firebase-admin';
-import * as serviceAccount from '../config/firebase-admin-key.json';
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
-});
-
-export { admin };
